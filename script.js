944eb114a16bccb660610d6b266a5a8ff6e33355
ismail oumai <ismail99game@gmail.com>
2026-02-24 19:43:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('ticketForm').addEventListener('submit', function(e) {
   const message = `🎉 Safari Vibes Pool Party Reservation 🎉%0AName: ${name}%0APhone: ${phone}%0ATicket: ${ticketType}%0AQuantity: ${quantity}`;
 
   // Replace with your WhatsApp number (example: 2126XXXXXXX)
-  const whatsappNumber = '212770429227';
+  const whatsappNumber = '212660735984';
 
   const url = `https://wa.me/${whatsappNumber}?text=${message}`;
   window.open(url, '_blank');
